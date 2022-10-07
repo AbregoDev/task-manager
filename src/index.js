ef891606b49065309c59afc7c95bf7f7d7947e83
BryanMorenoDev <bryan.moreno@tec.mx>
2022-10-06 19:46:12 -0500

    remove testing func

--- src/index.js
+++ src/index.js
@@ -15,18 +15,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server running on port', port);
 });
-
-const Task = require('./models/task');
-const User = require('./models/user');
-
-const main = async () => {
-    // const task = await Task.findById('6335ff2ad34e84a6f2e57594');
-    // await task.populate('owner');
-    // console.log(task);
-
-    // const user = await User.findById('63227bf4af3c8843c2e2b6f0');
-    // await user.populate('tasks');
-    // console.log(user.tasks);
-}
-
-main();
