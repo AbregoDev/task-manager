c450acc0231a09ba65b61c65d2fa796b9719d6f7
BryanMorenoDev <bryan.moreno@tec.mx>
2022-10-07 15:43:34 -0500

    install sharp optimize avatar storage

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const multer = require('multer');
+const sharp = require('sharp');
 
 const User = require('../models/user');
 const auth = require('../middleware/auth');
@@ -106,7 +107,11 @@ const upload = multer({
 });
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer)
+        .resize({ width: 250, height: 250 })
+        .png()
+        .toBuffer();
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
 }, (error, req, res, next) => {
